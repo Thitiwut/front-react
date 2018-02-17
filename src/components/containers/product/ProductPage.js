cb805e4714115cba6ca606cd49241773abefa79d
rootrapee.r <r.rattanapipit@gmail.com>
2018-02-17 20:26:28 +0700

    working on product page

--- src/components/containers/product/ProductPage.js
+++ src/components/containers/product/ProductPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { AddProductForm } from './AddProductForm';
+import { ProductTable } from './ProductTable';
 
 export class ProductPage extends React.Component {
 
@@ -8,11 +10,12 @@ export class ProductPage extends React.Component {
   render() {
     return (
       <div>
-      <h2 className="alt-header">Product</h2>
-      <p>
-        This example app is part of the <a href="https://github.com/coryhouse/react-slingshot">React-Slingshot
-        starter kit</a>.
-      </p>
+      <h2 className="alt-header">สินค้า</h2>
+      <AddProductForm
+      />
+      <h3>รายการสินค้า</h3>
+      <ProductTable
+      />
     </div>
     );
   }
